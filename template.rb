e6f00f5cf2efa090135fb4faf17b26b89c019b71
Ukaza Perdana <hello@ukazap.space>
2018-08-26 13:55:04 +0700

    Create template.rb

---
+++ template.rb
@@ -0,0 +1,13 @@
+gem 'komponent'
+gem 'slim-rails'
+gem 'webpacker'
+
+gem_group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'guard'
+  gem 'guard-livereload'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'rack-livereload'
+end
